2c1523d5178fb6d170d1ef5bb754587cab556cdd
Paul Okikijesu Afolabi <afolabip@accessbankplc.com>
2023-12-10 14:29:11 +0100

    Update output look

--- src/main/java/com/mycompany/gpacalculator/GPACalculator.java
+++ src/main/java/com/mycompany/gpacalculator/GPACalculator.java
@@ -25,12 +25,13 @@ public class GPACalculator {
         System.out.println("|----------------------------|-----------------------|------------|---------------------|---------------------|");
         System.out.println("|----- COURSE & CODE --------|----- COURSE UNIT -----|--- GRADE --|---- GRADE-UNIT -----|--- QUALITY POINTS --|");
         System.out.println("|----------------------------|-----------------------|------------|---------------------|---------------------|");
-        System.out.printf("| %s                          |           %d          |     %s     |           %d        |         %d          |\n", newStudent.course1, studentCreditUnit.creditUnit1, studentGrades[0], studentGradeUnit[0], studentQualityPoints[0]);
-        System.out.printf("| %s                          |           %d          |     %s     |           %d        |         %d          |\n", newStudent.course2, studentCreditUnit.creditUnit2, studentGrades[1], studentGradeUnit[1], studentQualityPoints[1]);
-        System.out.printf("| %s                          |           %d          |     %s     |           %d        |         %d          |\n", newStudent.course3, studentCreditUnit.creditUnit3, studentGrades[2], studentGradeUnit[2], studentQualityPoints[2]);
-        System.out.printf("| %s                          |           %d          |     %s     |           %d        |         %d          |\n", newStudent.course4, studentCreditUnit.creditUnit4, studentGrades[3], studentGradeUnit[3], studentQualityPoints[3]);
-        System.out.printf("| %s                          |           %d          |     %s     |           %d        |         %d          |\n", newStudent.course5, studentCreditUnit.creditUnit5, studentGrades[4], studentGradeUnit[4], studentQualityPoints[4]);
-        
+        System.out.printf("| %s                  |           %d          |     %s     |           %d        |         %d          |\n", newStudent.course1, studentCreditUnit.creditUnit1, studentGrades[0], studentGradeUnit[0], studentQualityPoints[0]);
+        System.out.printf("| %s                  |           %d          |     %s     |           %d        |         %d          |\n", newStudent.course2, studentCreditUnit.creditUnit2, studentGrades[1], studentGradeUnit[1], studentQualityPoints[1]);
+        System.out.printf("| %s                  |           %d          |     %s     |           %d        |         %d          |\n", newStudent.course3, studentCreditUnit.creditUnit3, studentGrades[2], studentGradeUnit[2], studentQualityPoints[2]);
+        System.out.printf("| %s                  |           %d          |     %s     |           %d        |         %d          |\n", newStudent.course4, studentCreditUnit.creditUnit4, studentGrades[3], studentGradeUnit[3], studentQualityPoints[3]);
+        System.out.printf("| %s                  |           %d          |     %s     |           %d        |         %d          |\n", newStudent.course5, studentCreditUnit.creditUnit5, studentGrades[4], studentGradeUnit[4], studentQualityPoints[4]);
+        System.out.println("|-------------------------------------------------------------------------------------------------------------|\n");
+
         System.out.printf("Your GPA is = %s to 2 decimal places", studentGPA);
     }
 }
